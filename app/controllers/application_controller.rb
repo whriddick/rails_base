60d6a6e02fbce981dbf0d270fb356d7c9e883e02
whriddick <whriddick@gmail.com>
2020-07-22 22:14:07 +0000

    HTML version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+
+def index
+  render({ :template => "layouts/index.html.erb"})
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
 
-
-
+# Homepage
+ get("/", {:controller => "application", :action =>"index" })
 end
